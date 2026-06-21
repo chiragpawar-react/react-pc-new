890e93aad1f39424a5fdaa2909b961d7a4bbeaca
react-pc <react-pc>
2026-06-21 15:32:54 +0530

    employee-management system done

--- employee-management system/src/components/Dashboard/EmployeeDashboard.jsx
+++ employee-management system/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../other/Header'
+import TastListNumbers from '../other/TastListNumbers'
+import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = (props) => {
   return (
-    <div>
-        <Header />
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
+      
+        <Header changeUser={props.changeUser} data={props.data} />
+        <TastListNumbers data={props.data} />
+        <TaskList data={props.data} />
     </div>
   )
 }
